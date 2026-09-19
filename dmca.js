c6a4e98ff2ab4b814696191b767fc50aac2be96d
cjsouthers1122 <cjsouthers1122@gmail.com>
2026-09-19 00:52:41 -0500

    Updated mintlify pages
    - Updated fingerprinting/overview.mdx
    - Updated fingerprinting/use-cases.mdx
    - Updated fingerprinting/process.mdx
    - Updated index.mdx
    - Created dmca.js
    
    Mintlify-Source: dashboard-editor

---
+++ dmca.js
@@ -0,0 +1,7 @@
+// Load DMCA.com badge helper script sitewide
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://images.dmca.com/Badges/DMCABadgeHelper.min.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
